b3c67da594c86e46cd9a8f7008bcfe3b71bce1e4
Simon Hildebrandt <simonhildebrandt@gmail.com>
2018-12-19 10:52:25 +1100

    Initial components

---
+++ src/index.js
@@ -0,0 +1,94 @@
+import socket from 'socket.io-client';
+import { Auth } from 'auth0tter';
+
+const noop = () => {}
+
+class Client {
+  constructor(auth) {
+    this.auth = auth;
+
+    this.socket = socket('ws://localhost:1979');
+    this.socket.on('connect', function(){
+      console.log('connected!', this.socket)
+    });
+    this.socket.on('message', function(data){
+      console.log('message!', data)
+    });
+    this.socket.on('error', (data) => {
+      console.log(data)
+       switch (data.type) {
+        case 'missing_auth':
+          console.error("Huh? we failed to send auth?")
+          break;
+        case 'invalid_auth':
+          console.log('time to log in again')
+          this.auth.logout();
+          break;
+        default:
+          console.error('unrecognised error from server', data)
+      }
+    });
+    this.socket.on('close', function(){
+      console.log('close!', this.socket)
+    });
+    this.socket.on('reconnect', () => {
+      console.log('reconnect!', this.socket)
+      this.notify(null, (x) => {console.log('arguments', x)});
+    });
+  }
+
+  notify(message = {}, callback=noop) {
+    console.log('sending', message)
+    this.socket.emit('message', {...message, auth: this.auth.token}, callback)
+  }
+
+  send(message) {
+    this.socket.send(message)
+  }
+}
+
+const auth = window.auth = new Auth({
+  domain: 'clockcamera.au.auth0.com',
+  clientID: 'Rc2vG1XNsWaQQDOQgzJ31DrUqAFxNool',
+  redirectUri: 'http://localhost:9000/index.html',
+  audience: 'CCServerDev', //https://clockcamera.au.auth0.com/userinfo',
+  responseType: 'token id_token',
+  scope: 'openid'
+})
+
+var button = document.getElementById('login')
+const client = window.client = new Client(auth)
+
+window.boot = function() {
+  button.addEventListener('click', (event) => {
+    if (window.auth.gotToken) {
+      window.auth.logout()
+    } else {
+      window.auth.login()
+    }
+  })
+
+  if (window.auth.gotToken) {
+    button.innerHTML = 'logout'
+  } else {
+    button.innerHTML = 'login'
+  }
+
+  window.auth.on('authenticated', () => {
+    button.innerHTML = 'logout'
+    client.notify()
+  })
+
+  window.auth.on('deauthenticated', () => {
+    button.innerHTML = 'login'
+  })
+
+  if (window.auth.gotToken){
+    client.notify()
+  }
+}
+
+window.conclude = function() {
+  console.log('concluding')
+  window.auth.conclude()
+}
